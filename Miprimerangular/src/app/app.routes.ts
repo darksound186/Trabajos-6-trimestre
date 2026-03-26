bb7ab709381b8d5f04b8275c39fe9d08a52c7fbc
Darikson Cortes Developer Junior <dariksoc@gmail.com>
2026-03-26 02:03:11 +0000

    modificaciones y creacion de carpetas

--- Miprimerangular/src/app/app.routes.ts
+++ Miprimerangular/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {} from './pages/inicio/inicio';
+import {} from './pages/ejercicios/ejercicios';
+import {} from './pages/contacto/contacto';
 
 export const routes: Routes = [];
